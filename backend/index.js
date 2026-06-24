b868e206462060cd38a231f31f5403031169f506
Parva Patel <parva.patel2005@gmail.com>
2026-06-24 16:49:37 +0530

    Done

--- backend/index.js
+++ backend/index.js
@@ -20,8 +20,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
-app.use(cors());
-
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use('/upload',express.static('upload'));
 
