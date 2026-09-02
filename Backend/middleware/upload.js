e9c16dfe4d1200edbb4a7f52e540759d63bdb52d
Parva Patel <parva.patel2005@gmail.com>
2026-09-02 23:20:41 +0530

    done

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -7,7 +7,7 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
-console.log(cloudinary.config());
+
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
