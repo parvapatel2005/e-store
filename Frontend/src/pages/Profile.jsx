b41aab596a347a422de4f5e454410058ac59fdc7
Parva Patel <parva.patel2005@gmail.com>
2026-09-03 00:48:50 +0530

    done

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -7,7 +7,6 @@ const Profile = () => {
         <h1>User Profile</h1>
         <h4>Full Name: {localStorage.getItem("userFullName")}</h4>
         <h4>Email: {localStorage.getItem("userEmail")}</h4>
-        <h4>Role: {localStorage.getItem("userRole")}</h4>
       </div>
     </>
   )
